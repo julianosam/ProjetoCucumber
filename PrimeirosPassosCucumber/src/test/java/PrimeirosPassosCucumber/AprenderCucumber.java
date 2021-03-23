fa032d4d8bbd70f24f0984274fbf9f6f50da03c5
leticiaperetti <leticiaperetti@hotmail.com>
2021-03-23 18:54:25 -0300

    Start projeto cucumber

---
+++ PrimeirosPassosCucumber/src/test/java/PrimeirosPassosCucumber/AprenderCucumber.java
@@ -0,0 +1,27 @@
+package PrimeirosPassosCucumber;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class AprenderCucumber {
+
+	@Dado("que criei o arquivo corretamente")
+	public void que_criei_o_arquivo_corretamente() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Quando("executa - lo")
+	public void executa_lo() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Então("a especificação deve finalizar com sucesso")
+	public void a_especificação_deve_finalizar_com_sucesso() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+
+
+}
