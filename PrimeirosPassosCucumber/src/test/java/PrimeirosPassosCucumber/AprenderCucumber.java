2d251841385073f02335904a1096cfcc8dcd7b6c
leticiaperetti <leticiaperetti@hotmail.com>
2021-03-23 19:43:51 -0300

    add dependencias

--- PrimeirosPassosCucumber/src/test/java/PrimeirosPassosCucumber/AprenderCucumber.java
+++ PrimeirosPassosCucumber/src/test/java/PrimeirosPassosCucumber/AprenderCucumber.java
@@ -7,19 +7,13 @@ import io.cucumber.java.pt.Quando;
 public class AprenderCucumber {
 
 	@Dado("que criei o arquivo corretamente")
-	public void que_criei_o_arquivo_corretamente() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void que_criei_o_arquivo_corretamente() {;
 	}
 	@Quando("executa - lo")
 	public void executa_lo() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
 	}
 	@Então("a especificação deve finalizar com sucesso")
 	public void a_especificação_deve_finalizar_com_sucesso() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
 	}
 	
 
