d08e192a65878f31bdd219bb8150d61d869f93ab
Juliano Sanches <juliano.sanches@adp.com>
2021-03-23 21:43:44 -0400

    Fixing package naming

--- PrimeirosPassosCucumber/src/test/java/com/leticia/cucumber/AprenderCucumber.java
+++ PrimeirosPassosCucumber/src/test/java/com/leticia/cucumber/AprenderCucumber.java
@@ -1,4 +1,4 @@
-package PrimeirosPassosCucumber;
+package com.leticia.cucumber;
 
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
@@ -7,15 +7,15 @@ import io.cucumber.java.pt.Quando;
 public class AprenderCucumber {
 
 	@Dado("que criei o arquivo corretamente")
-	public void que_criei_o_arquivo_corretamente() {;
+	public void que_criei_o_arquivo_corretamente() {
 	}
+
 	@Quando("executa - lo")
 	public void executa_lo() {
 	}
+
 	@Então("a especificação deve finalizar com sucesso")
 	public void a_especificação_deve_finalizar_com_sucesso() {
 	}
-	
-
 
 }
